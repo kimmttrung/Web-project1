93960485296c1cf35e623986cd96f8caf5d6f480
Mai Trung <maitantrung23012005@gmail.com>
2024-08-29 22:12:12 +0700

    add animation

---
+++ scripts/scripts.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+    ScrollReveal().reveal('.widget2-bottom', {
+        origin: 'bottom',
+        distance: '20px',
+        duration: 600,
+        easing: 'ease-in-out',
+        interval: 100,
+    });
+
+    ScrollReveal().reveal('.heading-left', {
+        origin: 'left',
+        distance: '200px',
+        duration: 1000,
+        easing: 'ease-in-out',
+        interval: 100,
+    });
+    ScrollReveal().reveal('.heading-left-lv2', {
+        origin: 'left',
+        distance: '200px',
+        duration: 1500,
+        easing: 'ease-in-out',
+        interval: 100,
+    });
+    ScrollReveal().reveal('.heading-right', {
+        origin: 'right',
+        distance: '200px',
+        duration: 600,
+        easing: 'ease-in-out',
+        interval: 100,
+    });
+    ScrollReveal().reveal('.heading-right-lv2', {
+        origin: 'right',
+        distance: '200px',
+        duration: 1000,
+        easing: 'ease-in-out',
+        interval: 100,
+    });
+
+});
